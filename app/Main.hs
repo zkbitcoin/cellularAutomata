b7405fc39969019ebcfadb488f025cc31ed1e2b7
zkbitcoin.com <admin@zkbitcoin.com>
2024-11-12 20:31:53 -0500

    update

--- app/Main.hs
+++ app/Main.hs
@@ -32,7 +32,7 @@ import Data.Colour.SRGB (sRGB)  -- Import sRGB to construct RGB color
 
 -- Define a foreign export function named 'generate'
 foreign export ccall test :: CInt -> CInt -> CString -> IO CInt
-foreign export ccall generate :: CInt -> CInt -> CString -> IO CInt
+foreign export ccall generate :: CString -> CInt -> CInt -> CString -> IO CInt
 
 test :: CInt -> CInt -> CString -> IO CInt
 test h w o = do
@@ -66,12 +66,11 @@ test h w o = do
     -- Return 0 to indicate success
     return 0
 
-generate :: CInt -> CInt -> CString -> IO CInt
-generate h w o = do
-    -- Convert the CString to a Haskell String
-    outputFile <- peekCString o
+generate :: CString -> CInt -> CInt -> CString -> IO CInt
+generate c h w o = do
 
-    -- Convert CInt to Int for height and width
+    caType <- peekCString c
+    outputFile <- peekCString o
     let height = fromIntegral h :: Int
     let width = fromIntegral w :: Int
 
@@ -97,9 +96,22 @@ generate h w o = do
             let sizeSpec = dims2D (fromIntegral width) (fromIntegral height)
             renderRasterific outpath sizeSpec diagram
 
-    -- Game of Life
-    -- ============
+    case caType of
+        "gol" -> do
+            -- Call the Game of Life generator and rendering function
+            putStrLn "Generating Game of Life GIF..."
+            generateGolGif caGifMain outputFile
+            putStrLn "Finished Generating Game of Life GIF..."
+            return 0  -- Successful execution
+        _ -> do
+            putStrLn $ "Unknown type: " ++ caType
+            return 1  -- Return 1 for failure or unknown type
 
+    return 0
+
+-- Game of Life generator and GIF rendering function
+generateGolGif :: (FilePath -> IO GameOfLife.GameOfLife -> Steps -> IO ()) -> FilePath -> IO ()
+generateGolGif caGifMain outputFile = do
     let golDim = 20
 
     let golGenerator :: IO GameOfLife.Cell
@@ -107,11 +119,10 @@ generate h w o = do
           val <- getStdRandom (randomR (0, 1)) :: IO Int
           return $ if val == 0 then GameOfLife.Off else GameOfLife.On
 
-    let golStartGrid :: IO (GameOfLife.GameOfLife)
+    let golStartGrid :: IO GameOfLife.GameOfLife
         golStartGrid = do
           univ <- makeUnivM golDim (const . const $ golGenerator)
           return $ GameOfLife.GameOfLife univ
 
+    -- Call caGifMain passed as argument
     caGifMain outputFile golStartGrid 100
-
-    return 0
